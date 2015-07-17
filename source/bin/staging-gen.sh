1ec9dc5470f6b1a0950ab3e911e59d03c7b51b0b
Richard Kellner <richard.kellner@erigones.com>
2015-07-17 18:39:41 +0200

    Switched html site into flask site, with script for html version generation

---
+++ source/bin/staging-gen.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+SOURCE_DIR="$(cd "$(dirname "$0")/.." ; pwd -P)"
+TMP_DIR="/tmp"
+GIT_ROOT="${SOURCE_DIR}/.."
+STAGING_DIR="staging"
+FLASK_URL="127.0.0.1:5000"
+
+# Go to flask templated site activate envs and run flask dev server
+cd "${SOURCE_DIR}"
+
+if [ -f envs/bin/activate ]; then
+	trap 'pkill -P $(jobs -pr) 2> /dev/null' SIGINT SIGTERM EXIT
+	source envs/bin/activate
+	python views.py &
+	echo "Running wget --mirror in 3 seconds..."
+	sleep 3
+else
+	echo "ENVS not found!"
+	exit 1
+fi
+
+# Store flask templated site as html
+cd "${TMP_DIR}"
+
+if [ -d "${FLASK_URL}" ]; then
+	rm -rf "${FLASK_URL}"
+fi
+
+wget -mkEp http://"${FLASK_URL}"/
+
+if [ $? -eq 0 ]; then
+	cd "${TMP_DIR}/${FLASK_URL}" || exit 3
+
+	# Restore static folder
+	rm -rf static
+	cp -a "${SOURCE_DIR}"/static . || exit 4
+
+	# Regenerate symlinks
+	ln -s static/download files
+
+	cd "${GIT_ROOT}"
+
+	# Move stored site as new site
+	if [ -d "${STAGING_DIR}" ]; then
+		rm -rf "${STAGING_DIR}"
+	fi
+
+	mv "${TMP_DIR}/${FLASK_URL}" "${STAGING_DIR}"
+
+else
+	echo "wget FAILED!"
+	exit 1
+fi
---
+++ source/views.py
@@ -0,0 +1,38 @@
+from flask import Flask, request, send_from_directory, render_template
+
+app = Flask(__name__, static_url_path='')
+
+@app.route('/')
+@app.route('/index.html')
+def index():
+    return render_template('index.html', title='Home')
+
+@app.route('/code-of-conduct.html')
+def code_of_conduct():
+    return render_template('code-of-conduct.html', title='Home')
+
+@app.route('/01-meetup.html')
+def meetup_01():
+    return render_template('01-meetup.html', title='Home')
+
+# Serve static files via flask so curl can collect them to build static pages
+
+@app.route('/static/js/<path:path>')
+def send_js(path):
+    return send_from_directory('static/js', path)
+
+@app.route('/static/css/<path:path>')
+def send_css(path):
+    return send_from_directory('static/css', path)
+
+@app.route('/static/images/<path:path>')
+def send_images(path):
+    return send_from_directory('/static/images', path)
+
+@app.route('/static/fonts/<path:path>')
+def send_fonts(path):
+    return send_from_directory('/static/fonts', path)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
