e334da41bae1238be876343aa40b7444e02b097f
Richard Kellner <richard@kellnerovci.sk>
2016-07-02 22:30:47 +0200

    Added 12th meetup

--- src/views.py
+++ src/views.py
@@ -788,7 +788,6 @@ def ba_meetup_10():
     return render_template('ba-10-meetup.html', **_get_template_variables(logo=LOGO_MEETUP_BA, li_meetup='active',
                                                                           ld_json=LDJSON_EVENT))
 
-
 @app.route('/<lang_code>/ba-11-meetup.html')
 def ba_meetup_11():
     lang =  get_locale()
@@ -818,7 +817,7 @@ def ba_meetup_11():
       ],
       "workPerformed": {
         "@type": "CreativeWork",
-        "name": u"Desiaty Bratislavský Python Meetup",
+        "name": u"Jedenásty Bratislavský Python Meetup",
         "creator": {
           "@type": "Organization",
           "name": "SPy o.z.",
@@ -835,6 +834,50 @@ def ba_meetup_11():
       }
     }
     return render_template('ba-11-meetup.html', **_get_template_variables(logo=LOGO_MEETUP_BA, li_meetup='active',
+                                                                          ld_json=LDJSON_EVENT))
+
+
+@app.route('/<lang_code>/ba-12-meetup.html')
+def ba_meetup_12():
+    lang =  get_locale()
+    LDJSON_EVENT = {
+      "@context": "http://schema.org",
+      "@type": "Event",
+      "name": u"Dvanásty Bratislavský Python Meetup",
+      "startDate": "2016-07-12T18:30:00+01:00",
+      "endDate" : "2016-07-12T20:00:00+01:00",
+      "url": "https://pycon.sk/"+ lang +"/ba-12-meetup.html",
+#      "sameAs": "https://www.facebook.com/events/110990379283146/",
+      "location": {
+        "@type": "Place",
+        "sameAs": "https://progressbar.sk",
+        "name": "Progressbar",
+        "address": u"Michalská 3, Bratislava"
+      },
+      "offers": {
+        "@type": "Offer",
+        "price": 0,
+        "priceCurrency": "EUR"
+      },
+      "workPerformed": {
+        "@type": "CreativeWork",
+        "name": u"Dvanásty Bratislavský Python Meetup",
+        "creator": {
+          "@type": "Organization",
+          "name": "SPy o.z.",
+          "url": "https://pycon.sk/"+ lang +"/spy.html",
+          "logo": "https://pycon.sk/static/images/pycon_sk_logo200_notext.png",
+          "sameAs": [
+            "https://facebook.com/pyconsk",
+            "https://twitter.com/pyconsk",
+            "https://www.linkedin.com/company/spy-o--z-",
+            "https://github.com/pyconsk",
+            "https://pyconsk.slack.com"
+          ],
+        }
+      }
+    }
+    return render_template('ba-12-meetup.html', **_get_template_variables(logo=LOGO_MEETUP_BA, li_meetup='active',
                                                                           ld_json=LDJSON_EVENT))
 
 def get_lastmod(route, sitemap_entry):
